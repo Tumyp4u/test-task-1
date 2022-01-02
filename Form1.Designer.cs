dea3b57e943ef920696bafc4006d0256202ca117
DariDie <daridie@mail.ru>
2022-01-02 12:56:37 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace test_task_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(23, 90);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(464, 23);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "Суммарная зарплата в разрезе департаментов без руководителей";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(23, 148);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(464, 23);
+            this.metroButton2.TabIndex = 1;
+            this.metroButton2.Text = "Департамент, в котором у сотрудника зарплата максимальна";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(23, 177);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(464, 23);
+            this.metroButton3.TabIndex = 2;
+            this.metroButton3.Text = "Зарплаты руководителей департаментов по убыванию";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.Location = new System.Drawing.Point(23, 119);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(464, 23);
+            this.metroButton4.TabIndex = 3;
+            this.metroButton4.Text = "Суммарная зарплата в разрезе департаментов с руководителями";
+            this.metroButton4.UseSelectable = true;
+            this.metroButton4.Click += new System.EventHandler(this.metroButton4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 233);
+            this.Controls.Add(this.metroButton4);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Name = "Form1";
+            this.Text = "Тестовое задание";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroButton metroButton4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using MetroFramework.Forms;
+using MetroFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Data.Sqlite;
+
+
+namespace test_task_1
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e) // Суммарную зарплату в разрезе департаментов без руководителей
+        {
+            string sqlExpression = "SELECT SUM(salary) FROM employee WHERE chief_id != 'NULL' GROUP BY department_id";
+            using (var connection = new SqliteConnection("Data Source=test_database.db"))
+            {
+                connection.Open();
+
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read())   // построчно считываем данные
+                        {
+                            var SUMsalary = reader.GetValue(0);
+                            MessageBox.Show($"{SUMsalary} \t");
+                        }
+                    }
+                }
+            }
+            Console.Read();
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e) // Департамент, в котором у сотрудника зарплата максимальна
+        {
+            string sqlExpression = "SELECT department.name, employee.department_id FROM department LEFT JOIN employee ON ( employee.department_id = department.id ) WHERE salary = (SELECT MAX(salary) FROM employee)";
+            using (var connection = new SqliteConnection("Data Source=test_database.db"))
+            {
+                connection.Open();
+
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read())   // построчно считываем данные
+                        {
+                            var department = reader.GetValue(0);
+                            MessageBox.Show($"{department} \t");
+                        }
+                    }
+                }
+            }
+            Console.Read();
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e) //Зарплаты руководителей департаментов (по убыванию)
+        {
+            string sqlExpression = "SELECT salary FROM employee WHERE chief_id IS NULL GROUP BY department_id ORDER BY salary DESC";
+            using (var connection = new SqliteConnection("Data Source=test_database.db"))
+            {
+                connection.Open();
+
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read())   // построчно считываем данные
+                        {
+                            var salary = reader.GetValue(0);
+                            MessageBox.Show($"{salary} \t");
+                        }
+                    }
+                }
+            }
+            Console.Read();
+        }
+
+        private void metroButton4_Click(object sender, EventArgs e) //Суммарную зарплату в разрезе департаментов c руководителями
+        {
+            string sqlExpression = "SELECT SUM(salary) FROM employee GROUP BY department_id";
+            using (var connection = new SqliteConnection("Data Source=test_database.db"))
+            {
+                connection.Open();
+
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read())   // построчно считываем данные
+                        {
+                            var SUMsalary = reader.GetValue(0);
+                            MessageBox.Show($"{SUMsalary} \t");
+                        }
+                    }
+                }
+            }
+            Console.Read();
+        }
+    }
+}
